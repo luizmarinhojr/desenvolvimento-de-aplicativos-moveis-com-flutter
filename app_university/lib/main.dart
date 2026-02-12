44eb72d364b29b4da0b91e6c4a5a4ae78f48a3b3
Luiz Carlos Marinho <luizcmoficial@gmail.com>
2026-02-12 20:10:13 -0300

    trabalho pratico - procedimento 7

--- app_university/lib/main.dart
+++ app_university/lib/main.dart
@@ -75,6 +75,18 @@ class MyApp extends StatelessWidget {
       ],
     );
 
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: const Text(
+        'O Lago Oeschinen fica aos pés do Blüemlisalp nos Alpes Berneses. Situado a 1.578'
+        'metros acima do nível do mar, é um dos lagos alpinos mais amplos. Um passeio de'
+        'teleférico a partir de Kandersteg, seguido por meia hora de caminhada por pastagens e'
+      'floresta de pinheiros, leva você ao lago, que aquece até 20 graus Celsius no verão. As'
+        'atividades desfrutadas aqui incluem remo e andar no tobogã de verão.',
+      softWrap: true,
+      ),
+    );
+
     return MaterialApp(
       title: 'Flutter layout demo',
       home: Scaffold(
@@ -85,6 +97,7 @@ class MyApp extends StatelessWidget {
           children: [
             titleSection,
             buttonSection,
+            textSection
           ],
         )
       ),
