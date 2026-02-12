c51ad22501dc41364d6966f9384b0e0c266caab0
Luiz Carlos Marinho <luizcmoficial@gmail.com>
2026-02-12 20:07:54 -0300

    trabalho pratico - procedimento 6

--- app_university/lib/main.dart
+++ app_university/lib/main.dart
@@ -43,6 +43,38 @@ class MyApp extends StatelessWidget {
       ),
     );
 
+    Column _buildButtonColumn(Color color, IconData icon, String label) {
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, color: color),
+          Container(
+            margin: const EdgeInsets.only(top: 8),
+            child: Text(
+              label,
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                color: color,
+              ),
+            ),
+          ),
+        ],
+      );
+    }
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+
     return MaterialApp(
       title: 'Flutter layout demo',
       home: Scaffold(
@@ -51,7 +83,8 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            titleSection
+            titleSection,
+            buttonSection,
           ],
         )
       ),
